2bb74c33ae3a5932c7b96a0126a94bf00e99878f
Bob <rbsavary88@gmail.com>
2023-04-20 13:52:08 -0400

    Views and serializers added

--- wealth_project/wealth_api/views.py
+++ wealth_project/wealth_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import WealthSerializer
+from .models import Wealth
+
 
 # Create your views here.
+class WealthList(generics.ListCreateAPIView):
+    queryset = Wealth.objects.all().order_by("id")
+    serializer_class = WealthSerializer
+
+
+class WealthDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Wealth.objects.all().order_by("id")
+    serializer_class = WealthSerializer
