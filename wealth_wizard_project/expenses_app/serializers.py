35cda93ed2c407910b3cf2f48a4a929cceabd9a6
Bob <rbsavary88@gmail.com>
2023-04-24 13:35:41 -0400

    ExpenseSerializer added

---
+++ wealth_wizard_project/expenses_app/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Expenses
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expenses
+        fields = ("id", "transaction", "amount", "date")
