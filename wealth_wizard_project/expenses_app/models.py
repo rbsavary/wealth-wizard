04d9d60bc7e269ccd8b180afde6e79cb46eca5ff
Bob <rbsavary88@gmail.com>
2023-04-24 14:27:16 -0400

    Updated project urls and added cors

--- wealth_wizard_project/expenses_app/models.py
+++ wealth_wizard_project/expenses_app/models.py
@@ -5,5 +5,5 @@ from datetime import datetime
 # Create your models here.
 class expense(models.Model):
     transaction = models.CharField(max_length=32)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    datetime = models.DateTimeField(default=datetime.now, blank=True)
     amount = models.FloatField()
