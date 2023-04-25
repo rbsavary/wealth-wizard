94513a81051980097e30a93f1b2a364cae311923
Bob <rbsavary88@gmail.com>
2023-04-25 12:36:22 -0400

    GET route added

--- wealth_wizard_frontend/src/App.js
+++ wealth_wizard_frontend/src/App.js
@@ -1,5 +1,22 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+let [expenses, setExpenses] = useState([])
+
+const getExpenses = () => {
+  axios
+    .get('http://localhost:/api/expenses')
+    .then(
+      (response) => setExpenses(response.data),
+      (err) => console.error(err)
+    )
+    .catch((error) => console.error(error))
+}
+
+useEffect(() => {
+  getExpenses()
+}, [])
 
 const App = () => {
   return (
@@ -8,7 +25,7 @@ const App = () => {
       
     </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
