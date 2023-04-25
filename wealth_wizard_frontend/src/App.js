7832747cc4a36a96d6f700d3ccf35d0574a872fc
Bob <rbsavary88@gmail.com>
2023-04-25 12:23:49 -0400

    Front end react app started

---
+++ wealth_wizard_frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  return (
+    <>
+    <div className="App">
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
